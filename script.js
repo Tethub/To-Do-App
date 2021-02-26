365681631dc60ed403e243dcb4338ddc47e295ed
deneme <beteljesed@gmail.com>
2021-02-26 17:46:48 +0300

    First step

---
+++ script.js
@@ -0,0 +1,128 @@
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
+const taskList = document.querySelector('#task-list');
+let items = [];
+
+
+loadItems();
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener('submit', addNewItem);
+    taskList.addEventListener('click', deleteItem);
+    btnDeleteAll.addEventListener('click', deleteAll);
+}
+
+function loadItems() {
+
+    items = getItemsFromLS();
+
+    items.forEach(function (item) {
+        createItem(item);
+    });
+}
+
+
+function getItemsFromLS() {
+    if (localStorage.getItem('items') === null) {
+        items = [];
+    } else {
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+
+    return items;
+}
+
+
+function setItemToLS(text) {
+    items = getItemsFromLS();
+    items.push(text);
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+
+function deleteItemFromLS(text) {
+    items = getItemsFromLS();
+    items.forEach(function (item, index) {
+        if (item === text) {
+            items.splice(index, 1);
+        }
+    });
+
+
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+function createItem(text) {
+
+    const li = document.createElement('li');
+    li.className = 'list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(text));
+
+    const a = document.createElement('a');
+    a.className = 'delete-item float-right';
+    a.setAttribute('href', '#');
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    li.appendChild(a);
+    taskList.appendChild(li);
+}
+
+function addNewItem(e) {
+
+    if (input.value === '') {
+        alert('You must add new item.');
+    }
+    else {
+
+        createItem(input.value);
+
+
+        setItemToLS(input.value);
+
+
+        input.value = '';
+
+
+    }
+
+
+    e.preventDefault();
+}
+
+
+function deleteItem(e) {
+
+    if (e.target.className === 'fas fa-times') {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+
+
+
+
+            deleteItemFromLS(e.target.parentElement.parentElement.textContent);
+
+        }
+    }
+
+    e.preventDefault();
+}
+
+
+function deleteAll(e) {
+
+    if (confirm('Are you sure?')) {
+
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+
+    localStorage.clear();
+
+
+    e.preventDefault();
+}
